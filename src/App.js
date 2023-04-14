6e26780b3694c995a4011eaf5d9db22517ebf53a
Vishnu <vishnurr1708@gmail.com>
2023-04-14 10:49:50 +0530

    check

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 function App(){
+
+  const datas = [
+    {
+      Name:"Age Calculator",
+      image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOvba_pnjsuDrCPy_Ta2Q1ZMgiUQZ5UmRMe0Ggv5cjvQ&usqp=CAU&ec=48665698",
+    },
+  ];
+
   return(
     <div className="bg-black h-screen w-screen">
       <div className="flex justify-center text-white py-14 text-3xl sm:text-5xl">
         <p className="drop-shadow-5xl shadow-white">Hii! Welcome to My UI-Designs</p>
+      </div>
+      <div className="text-white px-[70px] py-[50px] sm:px-[100px] sm:py-[100px] ">
+        Hii
       </div>
     </div>
   );
