e482d698fcca3d3854409c67d6ed192a4d429252
Vishnu <vishnurr1708@gmail.com>
2023-04-14 12:57:46 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -36,12 +36,12 @@ function App(){
       <div className="flex justify-center text-white py-[70px] text-3xl sm:text-5xl">
         <p className="drop-shadow-5xl shadow-white">Hii! Welcome to My UI-Designs</p>
       </div>
-      <div className="px-[85px] sm:py-10 sm:px-[150px] grid grid-row gap-10 sm:grid-cols-2 sm:gap-x-[100px] sm:gap-y-[70px]">
+      <div className="px-[50px] sm:py-10 sm:px-[150px] grid grid-row gap-10 sm:grid-cols-2 sm:gap-x-[100px] sm:gap-y-[70px]">
         {datas.map((i) => {
           return(
             <div key={i.Name}>
               <h3 className="text-3xl flex justify-center text-white">{i.Name}</h3>
-              <img src={i.image} alt="img" className="border-2 rounded-lg drop-shadow-5xl h-[320px] w-[400px] sm:h-[450px] sm:w-[550px] mt-5 hover:border-slate-800"></img>
+              <img src={i.image} alt="img" className="border-2 rounded-lg drop-shadow-5xl h-[320px] w-[500px] sm:h-[450px] sm:w-[550px] mt-5 hover:border-slate-800"></img>
             </div>
           );
         })}
