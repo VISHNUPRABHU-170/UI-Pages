dd66dc4bf91a678730c7abb228876e8950f8ccfe
Vishnu <vishnurr1708@gmail.com>
2023-04-14 10:30:08 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 function App(){
   return(
-    <div>
-      Hii
+    <div className="bg-black h-screen w-screen">
+      <div className="flex justify-center text-white py-14 text-3xl sm:text-5xl">
+        <p className="drop-shadow-5xl shadow-white">Hii! Welcome to My UI-Designs</p>
+      </div>
     </div>
   );
 }
