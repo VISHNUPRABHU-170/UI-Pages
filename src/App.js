f515dc0c668e5358230f98640c2f2137dfc2da00
Vishnu <vishnurr1708@gmail.com>
2023-04-14 12:50:42 +0530

    cards

--- src/App.js
+++ src/App.js
@@ -5,15 +5,46 @@ function App(){
       Name:"Age Calculator",
       image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOvba_pnjsuDrCPy_Ta2Q1ZMgiUQZ5UmRMe0Ggv5cjvQ&usqp=CAU&ec=48665698",
     },
+    {
+      Name:"Age Calculator",
+      image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOvba_pnjsuDrCPy_Ta2Q1ZMgiUQZ5UmRMe0Ggv5cjvQ&usqp=CAU&ec=48665698",
+    },
+    {
+      Name:"Age Calculator",
+      image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOvba_pnjsuDrCPy_Ta2Q1ZMgiUQZ5UmRMe0Ggv5cjvQ&usqp=CAU&ec=48665698",
+    },
+    {
+      Name:"Age Calculator",
+      image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOvba_pnjsuDrCPy_Ta2Q1ZMgiUQZ5UmRMe0Ggv5cjvQ&usqp=CAU&ec=48665698",
+    },
+    {
+      Name:"Age Calculator",
+      image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOvba_pnjsuDrCPy_Ta2Q1ZMgiUQZ5UmRMe0Ggv5cjvQ&usqp=CAU&ec=48665698",
+    },
+    {
+      Name:"Age Calculator",
+      image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOvba_pnjsuDrCPy_Ta2Q1ZMgiUQZ5UmRMe0Ggv5cjvQ&usqp=CAU&ec=48665698",
+    },
+    {
+      Name:"Age Calculator",
+      image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOvba_pnjsuDrCPy_Ta2Q1ZMgiUQZ5UmRMe0Ggv5cjvQ&usqp=CAU&ec=48665698",
+    },
   ];
 
   return(
-    <div className="bg-black h-screen w-screen">
-      <div className="flex justify-center text-white py-14 text-3xl sm:text-5xl">
+    <div className="bg-black">
+      <div className="flex justify-center text-white py-[70px] text-3xl sm:text-5xl">
         <p className="drop-shadow-5xl shadow-white">Hii! Welcome to My UI-Designs</p>
       </div>
-      <div className="text-white px-[70px] py-[50px] sm:px-[100px] sm:py-[100px] ">
-        Hii
+      <div className="px-[85px] sm:py-10 sm:px-[150px] grid grid-row gap-10 sm:grid-cols-2 sm:gap-x-[100px] sm:gap-y-[70px]">
+        {datas.map((i) => {
+          return(
+            <div key={i.Name}>
+              <h3 className="text-3xl flex justify-center text-white">{i.Name}</h3>
+              <img src={i.image} alt="img" className="border-2 rounded-lg drop-shadow-5xl h-[320px] w-[400px] sm:h-[450px] sm:w-[550px] mt-5 hover:border-slate-800"></img>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
